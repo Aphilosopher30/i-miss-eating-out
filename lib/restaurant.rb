c25530712a07cb5576bb59734e69f1e9bf2565d6
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-03 14:16:26 -0700

    refactoring method closing time

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -21,16 +21,38 @@ class Restaurant
     return @dishes
   end
 
+
+
+  def get_hour_from_time(time)
+    hours_vs_mn = time.split(":")
+    return hours_vs_mn[0]
+  end
+
+  def get_mn_from_time(time)
+    hours_vs_mn = time.split(":")
+    return hours_vs_mn[1]
+  end
+
+  def convert_time_to_string(hour, mn)
+    printable_time = hour + ":" + mn
+    return printable_time
+  end
+
   def closing_time(number)
     opens = self.opening_time
-    hours_vs_mn = opens.split(":")
-    opens_as_int = hours_vs_mn[0].to_i
-    closing_hour = opens_as_int + number
-    the_closeing_time = closing_hour.to_s + ":" + hours_vs_mn[1]
+    opening_hour = get_hour_from_time(opens)
+    opening_hour_int = opening_hour.to_i
+    opening_mn = get_mn_from_time(opens)
+
+    closing_hour_int = opening_hour_int + number
+    closing_hour = closing_hour_int.to_s
+
+    the_closeing_time = convert_time_to_string(closing_hour, opening_mn)
 
     return the_closeing_time
 
   end
+
 
   def add_dish(food)
     list_of_current_dishes = self.dishes
