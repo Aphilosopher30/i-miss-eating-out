56f48760c17ee09a0aaca2f92e5c68b39c9f0c72
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-03 13:50:11 -0700

    Iteration two

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -21,5 +21,23 @@ class Restaurant
     return @dishes
   end
 
+  def closing_time(number)
+    opens = self.opening_time
+    hours_vs_mn = opens.split(":")
+    opens_as_int = hours_vs_mn[0].to_i
+    closing_hour = opens_as_int + number
+    the_closeing_time = closing_hour.to_s + ":" + hours_vs_mn[1]
+
+    return the_closeing_time
+
+  end
+
+  def add_dish(food)
+    list_of_current_dishes = self.dishes
+    return list_of_current_dishes.append(food)
+  end
+
+
+
 
 end
